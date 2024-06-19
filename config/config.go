5ba151a51315cc7fa89ef1d4fe3bd682482daa95
Adrian Liechti <adrian@monobox.ch>
2024-06-19 21:57:12 +0200

    allow data path config

--- config/config.go
+++ config/config.go
@@ -1,33 +1,41 @@
 package config
 
 import (
-	"cassette/pkg/repository"
-	"cassette/pkg/repository/gorm"
-
 	"cassette/pkg/storage"
 	"cassette/pkg/storage/filesystem"
+	"os"
+	"path/filepath"
+
+	"cassette/pkg/repository"
+	"cassette/pkg/repository/gorm"
 )
 
 type Config struct {
-	Repository repository.Repository
 	Storage    storage.Storage
+	Repository repository.Repository
 }
 
 func FromEnvironment() (*Config, error) {
-	r, err := gorm.NewSQLite("sessions/db.sqlite3")
+	path := os.Getenv("DATA_PATH")
+
+	if path == "" {
+		path = "sessions"
+	}
+
+	s, err := filesystem.New(path)
 
 	if err != nil {
 		return nil, err
 	}
 
-	fs, err := filesystem.New("sessions")
+	r, err := gorm.NewSQLite(filepath.Join(path, "db.sqlite3"))
 
 	if err != nil {
 		return nil, err
 	}
 
 	return &Config{
+		Storage:    s,
 		Repository: r,
-		Storage:    fs,
 	}, nil
 }
